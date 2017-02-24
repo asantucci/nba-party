1c339e96a634406d2cb95ea2ddac6fd6a15b7d63
asantucci <santucci.andreas@gmail.com>
2017-02-24 12:12:28 -0800

    Preliminary results.

--- explore.r
+++ explore.r
@@ -24,17 +24,44 @@ data[, team := gsub('^[[:blank:]]+', '', team)]
 ### Relationship between 'new jersey nets' and 'brooklyn nets', collapse into one?
 data[grepl("\\<nets$", team), team := 'nets']
 
-data[, date := paste(date, year, sep = '/') %>% as.Date(x = ., format = '%m/%d/%Y')]
-data[, year := NULL]
-
 ### We lose 16 observations here...
 data[, spread := gsub('\'$', '', spread) %>% as.numeric]
 
-### Define what it means to party.
+
+data[, date := paste(date, year, sep = '/') %>% as.Date(x = ., format = '%m/%d/%Y')]
+data[, year := NULL]
+data[, day  := weekdays(date)]
+
+### Another 24 obs lost here.
+data <- data[!is.na(date)]
+
+
+### Number of days since last game.
+setkey(data, team, date)
+data[, last.game := c(NA, diff(date)), by = list(team, year(date))]
+data[, last.opp  := c(NA, lag(opponent), by = list(team, year(date)))]
+data[, last.loc  := c(NA, lag(location), by = list(team, year(date)))]
+
+##############################
+## What does it mean to party?
+##############################
+
 cities <- c("miami", "los angeles", "^la\\>", "^nets$", "brooklyn", "new york")
 rgx <- paste0('(', cities, ')', collapse = "|")
 
+### Generate a lag-variable which describes the location of the most-recent game played.
+setkey(data, team, date)
+
 data[, party := 0]
-data[grepl(rgx, team) & location == 'H', party := 1]
-data[grepl(rgx, opponent) & location == 'V', party := 1]
+
+### We (1) exclude people who already party, (2) check if last game played against party city
+### (3) check last game was played 'away' i.e. at party city and (4) party last night
+data[!grepl(rgx, team) & grepl(rgx, last.opp) & last.loc == 'V' & last.game == 1, party := 1]
+
+
+table(data$party, data$outcome)  # Only win 44% of the games.
+table(data[last.game == 1, outcome]) # Win 49% of the games they play the day before.
+
+
+
 
