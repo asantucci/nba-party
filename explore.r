a155808d019ec63a33879dd1824b6669c31a34d5
asantucci <santucci.andreas@gmail.com>
2017-02-26 14:10:43 -0800

    Included Atlanta as a party city. Looked at distribution of proportion of games for which a team-year meets the spread.

--- explore.r
+++ explore.r
@@ -27,14 +27,12 @@ data[grepl("\\<nets$", team), team := 'nets']
 ### We lose 16 observations here...
 data[, spread := gsub('\'$', '', spread) %>% as.numeric]
 
-
 data[, date := paste(date, year, sep = '/') %>% as.Date(x = ., format = '%m/%d/%Y')]
 data[, year := NULL]
 data[, day  := weekdays(date)]
 
 ### Another 24 obs lost here.
 data <- data[!is.na(date)]
-
 
 ### Number of days since last game.
 setkey(data, team, date)
@@ -46,7 +44,7 @@ data[, last.loc  := c(NA, lag(location), by = list(team, year(date)))]
 ## What does it mean to party?
 ##############################
 
-cities <- c("miami", "los angeles", "^la\\>", "^nets$", "brooklyn", "new york")
+cities <- c("miami", "los angeles", "^la\\>", "^nets$", "brooklyn", "new york", "atlanta")
 rgx <- paste0('(', cities, ')', collapse = "|")
 
 ### Generate a lag-variable which describes the location of the most-recent game played.
@@ -58,10 +56,11 @@ data[, party := 0]
 ### (3) check last game was played 'away' i.e. at party city and (4) party last night
 data[!grepl(rgx, team) & grepl(rgx, last.opp) & last.loc == 'V' & last.game == 1, party := 1]
 
+### We also consider: when the team had the day off the day before playing a party city
+data[grepl('V', location) & grepl(rgx, opponent) & last.game > 1 & !grepl(rgx, team), table(outcome)]
+
+### How often are teams meeting the spread?
+hist(data[, mean(outcome == 'W'), by = list(year(date), team)][, V1], breaks = seq(0.3, 0.8, by = 0.02), main = "How often are teams meeting the spread?", xlab = "Proportion of games for which a team-year meets the spread")
 
 table(data$party, data$outcome)  # Only win 44% of the games.
 table(data[last.game == 1, outcome]) # Win 49% of the games they play the day before.
-
-
-
-
