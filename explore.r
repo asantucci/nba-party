5eff3f60f8d319fbc3c1a951425b130f5e7e8ea8
asantucci <santucci.andreas@gmail.com>
2017-02-26 21:51:29 -0800

    Added a prototype logistic regression.

--- explore.r
+++ explore.r
@@ -24,25 +24,22 @@ data[, team := gsub('^[[:blank:]]+', '', team)]
 data[grepl("\\<nets$", team), team := 'nets']
 
 ### We lose 16 observations here...
+data <- data[spread != 'P']
 data[, spread := gsub('\'$', '', spread) %>% as.numeric]
 
-### DOUBLE CHECK THIS
 data[, year := NA_integer_]
-data[grepl("^1[01]/", date), year := substr(season, start = 1, stop = 4) %>% as.integer]
+data[grepl("^1[012]/", date), year := substr(season, start = 1, stop = 4) %>% as.integer]
 data[grepl("^[0-9]{1}/", date), year := substr(season, start = 6, stop = 10) %>% as.integer]
 
 data[, date := paste(date, year, sep = '/') %>% as.Date(x = ., format = '%m/%d/%Y')]
 data[, year := NULL]
 data[, day  := weekdays(date)]
 
-### Another 24 obs lost here.
-data <- data[!is.na(date)]
-
 ### Number of days since last game.
 setkey(data, team, date)
-data[, last.game := c(NA, diff(date)), by = list(team, year(date))]
-data[, last.opp  := c(NA, lag(opponent), by = list(team, year(date)))]
-data[, last.loc  := c(NA, lag(location), by = list(team, year(date)))]
+data[, last.game := c(NA, diff(date)),    by = list(team, season)]
+data[, last.opp  := c(NA, lag(opponent)[1:.N-1]), by = list(team, season)]
+data[, last.loc  := c(NA, lag(location)[1:.N-1]), by = list(team, season)]
 
 ##############################
 ## What does it mean to party?
@@ -55,11 +52,11 @@ party.rgx <- paste0('(', party.cities, ')', collapse = "|")
 setkey(data, team, date)
 
 ### 2009-2010
-bad.teams <- data.frame(season = '2009_2010',
-                        bad.teams = paste0('(', c('milwaukee', 'toronto', 'new york',
-                                                  'washington', 'memphis', 'minnesota',
-                                                  'oklahoma city', 'clippers', 'sacramento'),
-                                           ')', collapse = '|'))
+bad.2009 <- data.frame(season = '2009_2010',
+                       bad.teams = paste0('(', c('milwaukee', 'toronto', 'new york',
+                                                 'washington', 'memphis', 'minnesota',
+                                                 'oklahoma city', 'clippers', 'sacramento'),
+                                          ')', collapse = '|'))
 
 ### 2010-2011
 bad.2010 <- data.frame(season = '2010_2011',
@@ -106,7 +103,18 @@ data[!grepl(party.rgx, team) & grepl(party.rgx, last.opp) & last.loc == 'V' & la
 #data[grepl('V', location) & grepl(party.rgx, opponent) & last.game > 1, table(outcome)]
 
 ### How often are teams meeting the spread?
-hist(data[, mean(outcome == 'W'), by = list(season, team)][, V1], breaks = seq(0.325, 0.725, by = 0.02), main = "How often are teams meeting the spread?", xlab = "Proportion of games for which a team-year meets the spread")
+hist(data[, mean(outcome == 'W'), by = list(season, team)][, V1],
+     breaks = seq(0.3, 0.75, by = 0.025),
+     main = "How often are teams meeting the spread?",
+     xlab = "Proportion of games for which a team-year meets the spread")
 
 table(data$party, data$outcome)      # Only win 44% of the games.
 table(data[last.game == 1, outcome]) # Win 49% of the games they play the day before.
+
+model <- glm(outcome=='W' ~ last.game + party, family = 'binomial', data = data)
+
+### logistic
+### probability of meeting the spread ~ # days since last game + # games played in last week +
+###  + day of week + team-ranking + party city
+
+### proportion of games where spread is met if spread is over abs(X)
