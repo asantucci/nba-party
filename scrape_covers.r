22138b66d0c567fc7fb825981393aace48887614
asantucci <santucci.andreas@gmail.com>
2017-03-31 09:52:20 -0700

    Added fault-tolerance in the event of a failed http request, after realizing this was a significant problem.

--- scrape_covers.r
+++ scrape_covers.r
@@ -29,24 +29,33 @@ game.days <- gsub('-0([0-9])-', '-\\1-', game.days)
 ### Define Functions
 ##############################
 scrapeGame <- function(link) {
-    text <- readLines(link, warn = F)
-    ### We manually extract the date-field from HTML text.
-    date <- grep("\\<Date[^(]", text, value = T) %>% gsub('^.*- (.*)<br>', '\\1', .)
-    ### Determine the event-id from the link, such that we may scrape-lines.
-    event.id <- gsub('^.*boxscore([0-9]+).html$', '\\1', link)
-    lines <- paste0('http://www.covers.com/odds/linehistory.aspx?eventId=',
-                    event.id, '&sport=NBA')
-    ### Scrape lines, just grabbing one entry for now.
-    lines <- readHTMLTable(lines, header = T, which = 1)[1, ]  # <-- One entry assumption.
-    data  <- data.frame(date = date, lines)
-    setnames(data, c('date', 'line.ts', 'line', 'ou'))
-    return(data)
+    tryCatch(expr = {
+        text <- readLines(link, warn = F)
+        ### We manually extract the date-field from HTML text.
+        date <- grep("\\<Date[^(]", text, value = T) %>% gsub('^.*- (.*)<br>', '\\1', .)
+        ### Determine the event-id from the link, such that we may scrape-lines.
+        event.id <- gsub('^.*boxscore([0-9]+).html$', '\\1', link)
+        lines <- paste0('http://www.covers.com/odds/linehistory.aspx?eventId=',
+                        event.id, '&sport=NBA')
+        ### Scrape lines, just grabbing one entry for now.
+        lines <- readHTMLTable(lines, header = T, which = 1)[1, ]  # <-- One entry assumption.
+        data  <- data.frame(date = date, lines)
+        setnames(data, c('date', 'line.ts', 'line', 'ou'))
+        return(data)
+    }, error = function(e) {
+        Sys.sleep(1)
+        cat(paste0('Error when downloading from: ', link, '...trying again\n'))
+        scrapeGame(link)
+    })
 }
 
-scrapeCoverLines <- function(date) {
+scrapeCoverLines <- function(date, rescrape = F, save.path = 'raw_data/covers/') {
+    if (!rescrape && file.exists(paste0(save.path, date, '.csv')))
+        return(NULL)
+    cat(paste0("Now trying to download covers for date: ", date, "\n"))
     ### Bring up a listing of games played on a particular day.
-    pg <- paste0('http://www.covers.com/sports/NBA/matchups?selectedDate=', date)
-    tryCatch( {
+    tryCatch(expr = {
+        pg <- paste0('http://www.covers.com/sports/NBA/matchups?selectedDate=', date)
         doc <- htmlParse(pg)
         ### Each entry in games.played is a link to a detailed score-report.
         games.played <- xpathSApply(doc, "//a/@href") %>%
@@ -61,8 +70,13 @@ scrapeCoverLines <- function(date) {
         ### For each game played, scrape a listing of lines.
         lines <- lapply(games.played, scrapeGame) %>% rbindlist
         data <- cbind(teams, score, lines)
-        return(data)
-    }, error = function(e) return(NULL))
+        write.csv(data, file = paste0(save.path, date, '.csv'), row.names = F)
+        return(NULL)
+    }, error = function(e) {
+        Sys.sleep(1)
+        cat(paste0('Error when downloading data for date: ', date, '\n'))
+        scrapeCoverLines(date)
+    })
 }
 
 ##############################
@@ -75,13 +89,25 @@ clusterCall(cl, function() {
     require(magrittr)
     require(XML)
 })
+
 clusterExport(cl, 'scrapeGame')
 
 lines <- parLapplyLB(cl, game.days, scrapeCoverLines)
-tmp <- rbindlist(lines)
+## lines <- lapply(game.days, scrapeCoverLines)
+## save(lines, file = 'raw_data/covers.RData')
+lines <- rbindlist(lines)
+
+### Second pass?
+game.days <- as.Date(game.days)
+game.days <- setdiff(game.days %>% as.character, unique(lines$date) %>% as.character)
+game.days <- gsub('-0([0-9])-', '-\\1-', game.days)
+second.pass <- lapply(game.days, scrapeCoverLines)
+second.pass2 <- rbindlist(second.pass)
+save(second.pass2, file = 'tmp_data/second_pass.RData')
+lines <- rbind(lines, second.pass2)
 
 lines <- lines[, lapply(.SD, as.character), .SDcols = 1:ncol(lines)]
-save(lines, file = 'tmp_data/covers_lines.RData')
+save(lines, file = 'raw_data/covers.RData')
 
 lines[, game.time := gsub('^.* ([0-9:APM]+)$', '\\1', date)]
 lines[, date := as.Date(date, format = '%A, %B %d %Y')]
@@ -131,7 +157,7 @@ setnames(lines, 'team', 'home.team')
 lines <- merge(lines, teams, by.x = 'away', by.y = 'abbr', all.x = T)
 setnames(lines, 'team', 'away.team')
 
-lines <- lines[line != 'OFF', list(date, home.team, away.team, line)]
+lines <- lines[line != 'OFF', list(date, home.team, away.team, home.pts, away.pts, line)]
 lines[, line := as.numeric(line)]
 save(lines, file = 'tmp_data/covers_lines.RData')
 
