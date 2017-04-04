f1da2f02ceb05b471f27b9506f954acb2f51bc3f
asantucci <santucci.andreas@gmail.com>
2017-04-04 03:36:50 -0700

    Added code to merge in BLS data with corresponding teams.

--- clean_bls.r
+++ clean_bls.r
@@ -19,6 +19,9 @@
 ### Set up Workspace
 ##################################################
 
+require(data.table)
+require(ggmap)
+require(magrittr)
 require(parallel)
 
 cl <- makeCluster(detectCores())
@@ -28,6 +31,10 @@ clusterCall(cl, function() {
     require(data.table)
     require(magrittr)
 })
+
+##############################
+### Subset BLS Data into manageable chunks.
+##############################
 
 SubsetBLS <- function(year, raw.path = 'raw_data/bls/', save.path = 'tmp_data/bls/',
                       party = c('wine', 'spirit', 'beer', 'liquor', 'drinking')) {
@@ -54,3 +61,43 @@ SubsetBLS <- function(year, raw.path = 'raw_data/bls/', save.path = 'tmp_data/bl
 
 years <- 2010:2016
 parLapplyLB(cl, years, SubsetBLS)
+
+##################################################
+### Merge in BLS data with corresponding NBA teams.
+##################################################
+
+### Load BLS data.
+files <- list.files(path = 'tmp_data/bls', pattern = 'csv$', full.names = T)
+bls <- lapply(files, fread) %>% rbindlist
+
+### Load Covers data (simply to get a listing of team-names, it's a bit over-kill)
+load(file = 'tmp_data/covers_lines.RData')
+lines[, `:=`(pct = NULL, weekend = NULL)]
+others <- grep("party", names(lines), value = T, invert = T)
+setcolorder(lines, c(others, c('party')))
+
+### Geocode the locations for each of our NBA teams.
+teams <- unique(lines$team)
+locs  <- lapply(teams, geocode, output = 'more') %>% rbindlist(., fill = T)
+setnames(locs, gsub('_', '.', names(locs)))
+locs <- locs[, lapply(.SD, as.character), .SDcols = 1:ncol(locs)]
+
+### Be careful, since Washington Wizards are in Washington DC (so not a county)
+locs[, county := ifelse(is.na(administrative.area.level.2),
+                        administrative.area.level.1,
+                        paste(administrative.area.level.2,
+                              administrative.area.level.1, sep = ', '))]
+locs <- data.table(team = teams, county = locs$county)
+save(locs, file = 'tmp_data/team_locations.RData')
+
+drinks <- merge(locs, bls, by.x = 'county', by.y = 'area.title', all.x = T)
+
+drinks <- drinks[, list(ndrinking.estabs = mean(qtrly.estabs.count %>% as.numeric)),
+                 by = list(county, team, year)][order(year, ndrinking.estabs)]
+save(drinks, file = 'tmp_data/ndrink_estabs.RData')
+
+### Used to check that each team matches exactly one county in BLS data
+### (Except for Toronto Raptors)
+## for (t in locs$county)
+##     cat(paste0("Team ", t, " matched with ",
+##                bls[grep(t, area.title, ignore.case = T), length(unique(area.title))], '\n'))
