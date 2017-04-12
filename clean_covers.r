f100e1c0c5baf32f1bfa96dd6c7363aeabb4743e
asantucci <santucci.andreas@gmail.com>
2017-04-11 19:45:07 -0700

    Tidied up 'date' variable. Added points admitted and scored by each team.

--- clean_covers.r
+++ clean_covers.r
@@ -15,18 +15,6 @@
 ################################################################################
 ################################################################################
 
-### 1. Investigate party cities -- Increase quality of observations, be really confident they *stayed* in a party city overnight.
-###    Intuitively: you stayed in a city the night before where there is a lot of partying
-###    E.g. an East Coast team may fly into LA the day before and we may want to define that game to be a party city
-###    they may also fly out after an afternoon game and miss the party-effect completely.
-###    Data Sources: tweets
-###    Figure out: LA -- rest day -- Boston, we know they won't take the red-eye
-### 2. Weighting age by starting minutes
-### 3. Marital status
-
-### Bring game-time into data
-### Try and get a sense of what a travel schedule might be..
-
 ##################################################
 ### Set up Workspace, Read in Raw Data
 ##################################################
@@ -43,13 +31,13 @@ lines <- lapply(files, fread) %>% rbindlist
 ### Clean up date and line variables. Remove extraneous vars.
 ##################################################
 
-lines[, game.time := gsub('^.* ([0-9:APM]+)$', '\\1', date)]
-lines[, date := as.Date(date, format = '%A, %B %d %Y')]
+lines[, date := as.POSIXct(date, format = '%A, %B %d %Y %I:%M%p')]
+## lines[, game.time := gsub('^.* ([0-9:APM]+)$', '\\1', date)]
+## lines[, date := as.Date(date, format = '%A, %B %d %Y')]
 
 lines[, line := gsub('^(-?[0-9.]+)/.*$', '\\1', line)]
 lines[, line.ts := NULL]
 lines[, ou := NULL]
-## lines[, game.time := NULL]
 
 ##################################################
 ### Expand team abbreviations.
@@ -77,7 +65,7 @@ setnames(lines, 'team', 'away.team')
 ### Remove missing observations. Set line as numeric.
 ##################################################
 
-lines <- lines[line != 'OFF', list(date, game.time, home.team, away.team, home.pts, away.pts, line)]
+lines <- lines[line != 'OFF', list(date, home.team, away.team, home.pts, away.pts, line)]
 lines[, line := as.numeric(line)]
 
 ### Season spans the new year, i.e. starts in October ends in April.
@@ -97,7 +85,7 @@ lines[, outcome := ifelse(home.pts + line >= away.pts, 'W', 'L')]  # Ties?
 lines[, score := paste(home.pts, away.pts, sep = '-')]
 lines[, `:=`(home.pts = NULL, away.pts = NULL)]
 lines[, location := team]
-lines <- lines[, list(season, date, game.time, team, opponent, location, line, score, outcome)]
+lines <- lines[, list(season, date, team, opponent, location, line, score, outcome)]
 
 ##################################################
 ### Creating a panel-data set.
@@ -122,8 +110,9 @@ lines.dup[, `:=`(team = opponent, opponent = team,
 lines <- rbind(lines, lines.dup)
 
 setkey(lines, team, season, date)
-lines[, ndays.lgame := c(NA, diff(date)), by = list(team, season)]
-lines[, last.game.loc := c(NA, lag(location)[1:.N-1]), by = list(team, season)]
+lines[, nhours.lgame   := c(NA, diff(date)),              by = list(team, season)]
+lines[, last.game.loc  := c(NA, lag(location)[1:.N-1]),   by = list(team, season)]
+lines[, last.game.time := c(NA, lag(hour(date))[1:.N-1]), by = list(team, season)]
 
 ##################################################
 ### Distance Traveled.
@@ -158,24 +147,7 @@ standings[, pct := shift(pct, type = 'lag'), by = team]
 standings[, season := paste0(substr(season, 1, 2), substr(season, 6, 7)) %>% as.integer]
 
 lines <- merge(lines, standings, by = c('season', 'team'), all.x = T)
-###lines <- lines[lines[, head(.I, 82), by = list(season, team)]$V1] ### Excluded playoff games...
 
-##################################################
-### Party
-##################################################
-
-### Define a Party Variable
-lines[ndays.lgame == 1, list(meet.spread = mean(outcome == 'W'), .N), by = list(last.game.loc)][order(meet.spread)]
-## party.cities <- c('boston', 'brooklyn', 'dallas', 'los angeles')
-party.cities <- c('brooklyn', 'los angeles', 'new york')
-party.rgx <- paste0('(', party.cities, ')', collapse = "|")
-
-lines[, party := 0]
-lines[!grepl(party.rgx, team) & grepl(party.rgx, last.game.loc) & ndays.lgame == 1, party := 1]
-
-lines[, weekend := grepl("(Friday)|(Saturday)|(Sunday)", weekdays(date)) %>% as.integer]
-
-#save(lines, file = 'tmp_data/covers_lines.RData')
 ##################################################
 ### Average Age
 ##################################################
@@ -184,23 +156,18 @@ roster[, season := gsub('-', '_', season)]
 roster[, season := paste0(substr(season, 1, 2), substr(season, 6, 7)) %>% as.integer]
 setkey(roster, team, season)
 getAvgAge <- function(t, s, game.date, roster) {
-    roster[.(t, s), mean(game.date - birthdate)]
+    roster[.(t, s), mean(as.Date(game.date) - birthdate)]
 }
 lines[, avg.age := getAvgAge(team, season, date, roster), by = list(team, season, date)]
 lines[, avg.age := as.numeric(avg.age) %>% log]
 
+##############################
+### Save Data
+##############################
+
+lines[, c('team.pts.scored', 'team.pts.admitted') := tstrsplit(score, split = '-', fixed = T)]
+lines[, `:=`(team.pts.scored = as.numeric(team.pts.scored),
+             team.pts.admitted = as.numeric(team.pts.admitted))]
+
 save(lines, file = 'tmp_data/covers_lines.RData')
-
-
-
-
-
-
-
-
-
-
-
-
-
 
