39ec95e4ddfe04cce0fb59f50cc75189a82f6e18
asantucci <santucci.andreas@gmail.com>
2017-06-25 11:40:44 -0700

    Added functionality to scrape 'previews' of games, i.e. to get a schedule of games to bet on.

--- scrape_games_mlb.r
+++ scrape_games_mlb.r
@@ -15,6 +15,7 @@
 ################################################################################
 ################################################################################
 
+require(data.table)
 require(magrittr)
 require(RCurl)
 require(parallel)
@@ -40,7 +41,7 @@ getLinksToBoxScore <- function(year) {
                  year, '-schedule.shtml')
      htmlParse(pg) %>%
          xpathSApply(., path = '//a[@href]', xmlGetAttr, name = 'href') %>%
-         grep("boxes/[A-Z]{3}/[A-Z]{3}", ., value = T)    
+         grep("(boxes/[A-Z]{3}/[A-Z]{3})|(previews/[0-9]{4}/)", ., value = T) # Fetch both past and future games (for betting)
 }
 
 ### We go ahead and fetch links to box-scores for 2010:2017 seasons.
@@ -52,14 +53,26 @@ scrapeBoxScore <- function(link) {
     pg <- paste0('www.baseball-reference.com', link)
     tryCatch(expr = {
         pg <- getURL(pg) %>% htmlParse
-        teams <- readHTMLTable(pg, which = 1) %>%
-            `[`(., , 2)  %>%
-            as.character %>%
-            paste(., collapse = ' Vs. ')
-        game.info <- xpathSApply(pg, path = '//div[@class="scorebox_meta"]', xmlValue)
-        game.info <- strsplit(game.info, '[\r\n\t]+') %>%
-            unlist %>%
-            Filter(function(x) x != '', .)
+        ### If the game hasn't been played yet...
+        if (grepl('previews', link)) {
+            game.info <- xpathSApply(pg, path = '//h1', xmlValue) %>%
+                strsplit(.,  split = '[^0-9], ') %>% unlist
+            teams <- game.info[1]
+            months.regex <- month.name %>% paste0('(', .,  ')', collapse = '|')
+            date <- Filter(function(x) grepl(months.regex, x), game.info) %>%
+                gsub(' Matchup', '', .)
+            return(data.frame(matchup = teams, date = date, start.time = NA, attendance = NA,
+                              location = game.info[2], duration = NA))
+        } else {
+            teams <- readHTMLTable(pg, which = 1) %>%
+                `[`(., , 2)  %>%
+                as.character %>%
+                paste(., collapse = ' Vs. ')
+            game.info <- xpathSApply(pg, path = '//div[@class="scorebox_meta"]', xmlValue)
+            game.info <- strsplit(game.info, '[\r\n\t]+') %>%
+                unlist %>%
+                Filter(function(x) x != '', .)
+        }
         return(data.frame(matchup = teams,
                           date       = game.info[1], start.time = game.info[2],
                           attendance = game.info[3], location   = game.info[4],
