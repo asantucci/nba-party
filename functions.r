48086c23c1aeea613ac6552120e6bef28dbdb35f
asantucci <santucci.andreas@gmail.com>
2017-07-01 19:56:09 -0700

    Now using MSA's instead of counties.

--- functions.r
+++ functions.r
@@ -61,18 +61,17 @@ CleanBLS <- function(party.regex, suffix, sport, FUN, years = 2010:2016, RESCRAP
 ### Subset BLS data into manageable chunks, keeping only observations we care about.
 SubsetBLS <- function(year, party.regex, sport, suffix,
                       raw.path = 'raw_data/bls/', save.path = 'tmp_data/bls/') {
-    ### Create a listing of files for all relevant counties, load data.
+    ### Create a listing of files for all relevant MSA's, and load data.
     foldr <- list.files(path = raw.path, pattern = paste0(year, '.q1'), full.names = T)
     files <- list.files(path = foldr,
-                        pattern = '(county,)|(district of columbia)|(parish)',
+                        pattern = 'm(icro)?sa\\.csv',
                         full.names = T, ignore.case = T)
-    bls <- lapply(files, fread, colClasses = 'character',
-                  select = c('area_fips', 'year', 'qtr', 'industry_code', 'area_title',
-                             'industry_title', 'agglvl_title', 'size_title',
-                             'qtrly_estabs_count')) %>% rbindlist
+    bls <- lapply(files, fread, select = c('area_fips', 'year', 'qtr', 'industry_code',
+                                           'area_title', 'industry_title', 'agglvl_title',
+                                           'size_title', 'qtrly_estabs_count')) %>% rbindlist
     setnames(bls, gsub('_', '.', names(bls)))
     ### We will take our own total later, so for now we take granular data.
-    bls <- bls[grep("County, NAICS 6-digit", agglvl.title)]
+    bls <- bls[grep("NAICS 6-digit", agglvl.title)]
     ### Create regex to subset to drinking establshimensts.
     party.regex <- paste0('(', party.regex, ')', collapse = '|')
     bls <- bls[grep(party.regex, industry.title, ignore.case = T)]
