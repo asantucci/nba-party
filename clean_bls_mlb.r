d2e2a21fba5aa8ead98d5194f1d49d03eca0e323
asantucci <santucci.andreas@gmail.com>
2017-07-03 02:09:08 -0700

    Outputting data for comparison plot.

--- clean_bls_mlb.r
+++ clean_bls_mlb.r
@@ -8,6 +8,13 @@
 ###
 ### Date: July 2017
 ###
+### Inputs: 'raw_data/bls/20[xx].q1-q4.by_area'
+###
+### Output: 'tmp_data/bls/[suffix]_[sport].RData'
+###         e.g. 'studios_and_artists_mlb.RData'
+###
+### Dependencies: data.table, ggmap, magrittr, parallel
+###
 ################################################################################
 ################################################################################
 
@@ -27,5 +34,10 @@ clusterCall(cl, function() {
 ### All the heavy lifting is done in our CleanBLS function...
 source('functions.r')
 
+### Load raw data, subset based on party.regex, aggregate using FUN, and save using suffix/sport.
 CleanBLS(party.regex = c('sound recording', 'music publisher', 'musical group'),
          suffix = 'studios_and_artists', sport = 'mlb', FUN = sum)
+
+### Avoids rescraping data. This output used for a comparison plot in the paper.
+CleanBLS(party.regex = c('alcohol', 'drinking'),
+         suffix = 'alcohol_and_drinking', sport = 'mlb', FUN = sum, RESCRAPE = F)
