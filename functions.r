2bfaf8c9e3c054c2b33417fb4b00a404065540b4
asantucci <santucci.andreas@gmail.com>
2017-07-03 02:21:05 -0700

    Bookmarked legacy code.

--- functions.r
+++ functions.r
@@ -129,7 +129,11 @@ Bet <- function(our.prediction, actual.outcome, odds) {
         return(-100)
 }
 
+##################################################
+### Legacy code.
+##################################################
 
+### Fetch is used as a helper function within CollectData (below).
 Fetch <- function(team, beg, end, data, year) {
     datum <- data[beg:end]
     ### We have to be careful for data entry errors. Ex: missing delimiter.
@@ -141,34 +145,25 @@ Fetch <- function(team, beg, end, data, year) {
         data.table(.) %>% cbind(team)
 }
 
-
+### CollectData was used in our preliminary analysis when we had data from gold-lines.com
 collectData <- function(datafile) {
     data <- readLines(datafile) # 'raw_data/2011.txt')
-
     ### Remove blank lines.
     blanks <- grep("^[[:blank:]]*$", data)
     data <- data[-blanks]
-
     ### Find team names, aka starting indices. 
     beg <- grep("^\\(SUR", data) - 1     # Look one row before team-summary for team-name.
     end <- c(beg[-1] - 1, length(data))  # Find limits of data for each team.
     teams <- data[beg]
-
     ### This data frame describes where data start-and-stop for each team.
     teams <- data.frame(team = teams, beg = beg+2, end = end)
-
     data <- mapply(Fetch, team = teams$team, beg = teams$beg, end = teams$end, year = '2011',
                    MoreArgs = list(data=data), SIMPLIFY = F)
-
     data <- rbindlist(data)
     data[['season']] = gsub('raw_data/([0-9_]{9})\\.txt', '\\1', datafile)
-
     setnames(data, c('date', 'opponent', 'outcome', 'spread', 'score',
                      'location', 'ou', 'team', 'season'))
     setcolorder(data, c('season', 'date', 'team', 'opponent', 'outcome', 'spread', 'score',
                         'location', 'ou'))
     return(data)
 }
-
-## getFullTeamName <- function(needle, haystack)
-##     grep(needle, haystack, value = T)
