4f362e16d383eafa64b25ca0f193bccb12eb8ed2
asantucci <santucci.andreas@gmail.com>
2017-07-04 11:47:08 -0700

    Now using hangover package. Nothing else new.

--- scrape_standings.r
+++ scrape_standings.r
@@ -1,29 +1,27 @@
+################################################################################
+################################################################################
+################################################################################
+###
+### Title: Scrape ESPN Team Standings
+###
+### Andreas Santucci
+###
+### Date: July 2017
+###
+### Inputs: 
+###
+### Dependencies: 
+###
+################################################################################
+################################################################################
+
+require(hangover)
 
 require(data.table)
 require(magrittr)
 require(XML)
 
-scrapeESPNStandings <- function(year) {
-    pg <- paste0('http://www.espn.com/nba/standings/_/season/', year, '/group/league')
-    dt <- htmlTreeParse(pg, useInternalNodes = T)
-    season   <- xpathSApply(doc = dt, path = '//div//section//header//h1', xmlValue)
-    rankings <- getNodeSet(doc = dt, path = '//div//table//tr[@class=" standings-row"]')
-    team.names <- xpathSApply(doc = dt, path = '//tr[@class=" standings-row"]//span//span', xmlValue)
-    fields <- xpathSApply(doc = dt, path = '//tr[@class=" standings-row"]//td', xmlValue)
-    breaks <- grep(paste0('(', team.names, ')', collapse = '|'), fields)
-    nfields <- unique(diff(breaks))
-    stopifnot(length(nfields) == 1)
-    fields <- lapply(seq_along(breaks), function(i)
-        fields[breaks[i]:(breaks[i]+nfields-1)])
-    fields <- do.call(rbind, fields)
-    data <- data.table(fields)
-    setnames(data, c('team', 'wins', 'losses', 'pct', 'gb', 'home', 'road', 'div', 'conf', 'ppg', 'opp.ppg', 'diff', 'strk', 'l10'))
-    data[, team := team.names]
-    data[, season := season]
-    return(data)
-}
-
-standings <- lapply(2005:2017, scrapeESPNStandings)
+standings <- lapply(2005:2017, hangover::scrapeESPNStandings)
 standings <- rbindlist(standings)
 standings[, season := gsub('NBA Standings - ', '', season)]
 
