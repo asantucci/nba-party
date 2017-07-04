68291ffad78b7fac6069087e151c0feba16f84e5
asantucci <santucci.andreas@gmail.com>
2017-07-03 23:54:07 -0700

    Now using reusable functions from R package 'hangover'.

--- scrape_covers.r
+++ scrape_covers.r
@@ -21,6 +21,7 @@
 ### Set up Workspace
 ##############################
 
+require(hangover)
 require(parallel)
 
 cl <- makeCluster(detectCores())
@@ -37,67 +38,10 @@ game.days <- Filter(function(x) x < Sys.Date() & x > '2010-11-25', game.days)
 game.days <- gsub('-0([0-9])-', '-\\1-', game.days)
 
 ##############################
-### Define Functions
-##############################
-scrapeGame <- function(link) {
-    tryCatch(expr = {
-        text <- readLines(link, warn = F)
-        ### We manually extract the date-field from HTML text.
-        date <- grep("\\<Date[^(]", text, value = T) %>%
-            gsub('^.*- (.*)<br>', '\\1', .)
-        ### Determine the event-id from the link, such that we may scrape-lines.
-        event.id <- gsub('^.*boxscore([0-9]+).html$', '\\1', link)
-        lines <- paste0('http://www.covers.com/odds/linehistory.aspx?eventId=',
-                        event.id, '&sport=NBA')
-        ### Scrape lines, just grabbing one entry for now.
-        lines <- readHTMLTable(lines, header = T, which = 1)[1, ]  # <-- One entry.
-        data  <- data.frame(date = date, lines)
-        setnames(data, c('date', 'line.ts', 'line', 'ou'))
-        return(data)
-    }, error = function(e) {
-        Sys.sleep(1)
-        cat(paste0('Error when downloading from: ', link, '...trying again\n'))
-        scrapeGame(link)
-    })
-}
-
-scrapeCoverLines <- function(date, rescrape = F, save.path = 'raw_data/covers/', ntries = 0) {
-    if (ntries > 5) return(NULL)
-    if (!rescrape && file.exists(paste0(save.path, date, '.csv')))
-        return(NULL)
-    cat(paste0("Now trying to download covers for date: ", date, "\n"))
-    ### Bring up a listing of games played on a particular day.
-    tryCatch(expr = {
-        pg <- paste0('http://www.covers.com/sports/NBA/matchups?selectedDate=', date)
-        doc <- htmlParse(pg)
-        ### Each entry in games.played is a link to a detailed score-report.
-        games.played <- xpathSApply(doc, "//a/@href") %>%
-            grep('boxscore', ., value = T)
-        ### Games.stats contains quarter-by-quarter scores, alongside team-names.
-        games.stats <- readHTMLTable(doc)
-        teams <- t(sapply(games.stats, function(x) x[[1]] %>% as.character)) 
-        score <- t(sapply(games.stats, function(x) x[[ncol(x)-1]])) # Pt totals.
-        colnames(teams) <- c('away', 'home')
-        colnames(score) <- c('away.pts', 'home.pts')
-        rownames(teams) <- NULL
-        ### For each game played, scrape a listing of lines.
-        lines <- lapply(games.played, scrapeGame) %>% rbindlist
-        data <- cbind(teams, score, lines)
-        write.csv(data, file = paste0(save.path, date, '.csv'), row.names = F)
-        return(NULL)
-    }, error = function(e) {
-        Sys.sleep(1)
-        cat(paste0('Error when downloading data for date: ', date, '\n'))
-        scrapeCoverLines(date, ntries = ntries + 1)
-    })
-}
-
-##############################
 ### Scrape
 ##############################
 
 ### Note that we must export scrapeCoverLines since in the event of a fault,
 ### an individual core will need to re-call this function.
-clusterExport(cl, c('scrapeGame', 'scrapeCoverLines'))
-lines <- parLapplyLB(cl, game.days, scrapeCoverLines)
-#lapply(game.days, scrapeCoverLines)
+clusterExport(cl, c('hangover::scrapeGame', 'hangover::scrapeCoverLinesNBA'))
+lines <- parLapplyLB(cl, game.days, hangover::scrapeCoverLinesNBA)
--- scrape_covers_mlb.r
+++ scrape_covers_mlb.r
@@ -21,6 +21,8 @@
 ### Set up workspace
 ##############################
 
+require(hangover)
+
 require(data.table)
 require(magrittr)
 require(parallel)
@@ -29,50 +31,6 @@ load(file = 'tmp_data/game_days_mlb.RData')
 
 game.days <- Filter(function(x) x > as.Date('2011-01-01'),
                     x = game.days) %>% as.character
-
-##############################
-### Define functions
-##############################
-
-### Scrapes lines data for a particular day.
-### At the moment, just returns one betting observation.
-scrapeLines <- function(link) {
-    tryCatch(expr = {
-        lines <- readHTMLTable(link, which = 1)
-        if (is.null(lines))
-            return(data.frame("no", "lines", "data"))
-        else
-            head(lines, 1)
-    }, error = function(e) return(data.frame("no", "lines", "data")))
-}
-
-### For a particular date,this function fetches a listing of games
-### that occurred, alongside their corresponding outcome/score and line.
-scrapeListingOfGames <- function(date) {
-    tryCatch(expr = {
-        pg <- paste0('http://www.covers.com/sports/MLB/matchups?selectedDate=', date)
-        lines.links <- htmlParse(pg) %>%
-            xpathSApply(., '//a/@href') %>%
-            grep('linehistory', ., value = T, ignore.case = T)
-        ### Here, we are careful with games that got postponed (i.e. scheduled to play but no outcome).
-        game.ids.with.scores <- htmlParse(pg) %>%
-            xpathSApply(., '//a/@href') %>%
-            grep('boxscore', ., value = T, ignore.case = T) %>%
-            gsub('^.*boxscore([0-9]+)\\.html$', '\\1', .)
-        game.ids.with.scores <- paste0('(', game.ids.with.scores, ')', collapse = '|')
-        lines.links <- Filter(function(x) grepl(game.ids.with.scores, x), lines.links)
-        games.stats <- readHTMLTable(pg)
-        teams <- sapply(games.stats, function(x)
-            x[, 1] %>% as.character  %>% paste(., collapse = '-'))
-        scores <- sapply(games.stats, function(x)
-            x[, 'R'] %>% as.character %>% paste(., collapse = '-'))
-        lines <- lapply(lines.links, scrapeLines) %>% rbindlist
-        data <- data.frame(date = date, matchup = teams, lines, score = scores)
-        write.csv(data, file = paste0('tmp_data/covers_mlb/', date, '.csv'),
-                  row.names = F)
-        cat(paste0("Wrote to disk data for date: ", date, "\n"))
-    }, error = function(e) return(NULL))
-}
 
 ##############################
 ### Scrape!
@@ -97,7 +55,6 @@ if (!RESCRAPE) {
 }
 
 ### Scrape.
-#parLapplyLB(cl, game.days, scrapeListingOfGames)
-lapply(game.days, scrapeListingOfGames)  # May have to run this a few times to scrape all files.
+lapply(game.days, scrapeCoverLinesMLB)  # May have to run this a few times to scrape all files.
 
 
