d2ab6689a86335a7839cb75b6342e921a100e00a
asantucci <santucci.andreas@gmail.com>
2017-07-07 15:45:27 -0700

    Removed extraneous code.

--- hangover/R/CleanBLS.r
+++ hangover/R/CleanBLS.r
@@ -24,34 +24,4 @@ CleanBLS <- function(party.regex, suffix, sport, FUN, years = 2010:2016) {
               by = list(area.title, year)]
     dt[, year := as.integer(year)]
     save(dt, file = paste0('tmp_data/', suffix, '.RData'))
-    ## ### Fetch a listing of team-names from our points-spread/money-lines data. (overkill)
-    ## if (sport == 'nba') {
-    ##     load(file = 'tmp_data/covers_lines.RData')
-    ##     lines[, pct := NULL]
-    ##     teams <- unique(lines$team)
-    ## } else if (sport == 'mlb') {
-    ##     abbrs <- fread(file = 'mlb_abbrs.csv')
-    ##     setnames(abbrs, c('abbr', 'full', 'nick'))
-    ##     teams <- abbrs$full
-    ## }
-    ## ### Geocode the location of each team within the sport, and add this to our BLS data.
-    ## locs <- MyGeoCode(teams, sport)
-    ## bls[, area.title := gsub(' msa$', '', area.title, ignore.case = T)]
-    ## states <- data.table(state = state.name, state.abb = state.abb)
-    ## states <- rbind(states, data.frame(state = 'District of Columbia', state.abb = 'DC'))
-    ## locs <- locs[states, on = 'state', nomatch = 0]
-    ## ### We can't simply merge. We have to look for the county within the MSA.
-    ## dt <- lapply(1:nrow(locs), function(i) { 
-    ##     matched <- bls[grepl(locs[i, locality],  area.title, ignore.case = T) &
-    ##                    grepl(locs[i, state.abb], area.title, ignore.case = T)]
-    ##     matched[, c('locality', 'state', 'team') := locs[i, list(locality, state.abb, team)]]
-    ##     return(matched)
-    ## }) %>% rbindlist
-    ## dt <- dt[, list(variable = FUN(qtrly.estabs.count %>% as.numeric)),
-    ##          by = list(locality, state, year)][order(year, variable)]
-    ## setnames(dt, 'year', 'season')
-    ## dt[, season := as.numeric(season)]
-    ## ### NBA season spans new-years, e.g. change 2010-11 season --> 2011.
-    ## if (sport == 'nba') dt[, season := season + 1]
-    ## save(dt, file = paste0('tmp_data/', suffix, '_', sport, '.RData'))
 }
